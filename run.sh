7d8c5560c49c2f74911793f33cdc3744d4ba831c
Miscro V <miscrov@outlook.com>
2026-02-20 07:32:56 -0600

    docs for shell script

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 !/bin/sh
 
-# Compile the C program
+# Exit on failure
 set -e
 
+# Build the project 
 (
 cd "$(dirname "$0")"
 cmake -S . -B build
 cmake --build build
 )
 
+# Run the executable with any provided arguments
 exec $(dirname "$0")/build/networkin "$@"
